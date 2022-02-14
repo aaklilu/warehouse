b7a81e2cd901ffa061bce2f5a12f66c0743e7be0
Anteneh Aklilu <reachanteneh@gmail.com>
2022-02-14 21:14:31 +0100

    feat: Setup warehopuse application and design API * Design Product and Orders APIs

--- src/test/kotlin/com/example/warehouse/WarehouseApplicationTests.kt
+++ src/test/kotlin/com/example/warehouse/WarehouseApplicationTests.kt
@@ -2,10 +2,31 @@ package com.example.warehouse
 
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
-class WarehouseApplicationTests: IntegrationTest() {
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+class WarehouseApplicationTests {
     companion object {
         private val logger = LoggerFactory.getLogger(WarehouseApplicationTests::class.java)
+
+        @Container
+        private val postgresContainer = PostgreSQLContainer<Nothing>("postgres:latest")
+
+        @DynamicPropertySource
+        @JvmStatic
+        fun registerDynamicProperties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgresContainer::getJdbcUrl)
+            registry.add("spring.datasource.username", postgresContainer::getUsername)
+            registry.add("spring.datasource.password", postgresContainer::getPassword)
+        }
     }
 
     @Test
